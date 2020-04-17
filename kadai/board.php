af0e80f97fe8d40786867e26f20482a6609358b8
tomukobayashi <scyoutec.34@gmail.com>
2020-04-17 18:11:00 +0900

    first commit

---
+++ kadai/board.php
@@ -0,0 +1,31 @@
+<?php
+require_once('./functions.php');
+
+$data=[];
+$errs = [];
+//データベースへの接続
+$dbh = get_db_connect();
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    //POSTデータの取得
+    $name = get_post('name');
+    $comment = get_post('comment');
+    //文字数のチェック
+    if (!check_words($name, 50)) {
+        $errs[] = 'お名前欄を修正してください';
+    }
+    if (!check_words($comment, 200)) {
+        $errs[] = 'コメント欄を修正してください';
+    }
+
+    if(count($errs) === 0){
+    //コメントの書き込み
+    $result = insert_comment($dbh,$name,$comment);
+    }
+}
+
+//全コメントデータの取得
+$data = select_comments($dbh);
+
+require_once('view.php');
+
+
